bcab53e23a03179bf852fc41a62aae80d55502c4
David González León <david.gonzalezleon@heig-vd.ch>
2021-05-11 09:13:37 +0200

    added server submodule

---
+++ Client/ServerTest/src/main/java/ch/heigvd/robotpi/servertest/Starter.java
@@ -0,0 +1,12 @@
+/*
+ * @File Starter.java
+ * @Authors : David González León
+ * @Date 11 mai 2021
+ */
+package ch.heigvd.robotpi.servertest;
+
+public class Starter {
+   public static void main(String[] args) {
+
+   }
+}
