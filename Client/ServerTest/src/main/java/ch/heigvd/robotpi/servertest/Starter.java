b299b6b652b7fecd0c844149c3575825ad1d4daa
David González León <david.gonzalezleon@heig-vd.ch>
2021-05-11 10:05:34 +0200

    added ability to launch server as standalone class

--- Client/ServerTest/src/main/java/ch/heigvd/robotpi/servertest/Starter.java
+++ Client/ServerTest/src/main/java/ch/heigvd/robotpi/servertest/Starter.java
@@ -5,9 +5,28 @@
  */
 package ch.heigvd.robotpi.servertest;
 
+import java.util.Scanner;
+
 public class Starter {
    public static void main(String[] args) {
-      Server server = new Server(2025, "good");
-      server.run();
+      Server server = new Server(2025, "good", false);
+      Thread thread = new Thread(server);
+      thread.start();
+      System.out.println("Enter \"stop\" to stop the server from this terminal");
+      Scanner scanner = new Scanner(System.in);
+      while (true) {
+         String response = scanner.nextLine();
+         if (response.equals("stop")){
+            System.out.println("Stopping execution of the server");
+            server.stopExecution();
+            try {
+               thread.join();
+            } catch (InterruptedException e) {
+               e.printStackTrace();
+            }
+            break;
+         }
+      }
+      System.out.println("Server stopped, goodbye!");
    }
 }
