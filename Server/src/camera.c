130647c3824d7f8969097729086eb748c0a846f5
Anthony Jaccard <to.jaccard@hotmail.ch>
2021-05-12 16:10:50 +0200

    Function implementation

--- Server/src/camera.c
+++ Server/src/camera.c
@@ -1,6 +1,19 @@
 #include <camera.h>
 
+CamSettings settings = {512, 512, JPG, 0};
+
+CamSettings getCamSettings()
+{
+   return settings;
+}
+
+void setCamSettings(CamSettings newSettings)
+{
+   settings = newSettings;
+}
+
 unsigned char  camSnap(char *nameBuf, const unsigned char nameSize, const char *folderName, const unsigned char  folderNameSize)
 {
+   char command[128];
    return 0;
 }
